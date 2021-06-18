10941a9980c58fa9d1af9cc463b4a5381e309251
SpdDev <spd@tutanota.com>
2021-06-18 15:58:54 +0400

    add sounds to game

--- shmup/shmup.py
+++ shmup/shmup.py
@@ -5,6 +5,8 @@ from os import path
 
 # Задаём путь до каталога с изображениями
 img_dir = path.join(path.dirname(__file__), 'img')
+# Задаём путь до каталога со звуками
+snd_dir = path.join(path.dirname(__file__), 'snd')
 
 # параметры игрового окна
 WIDTH = 480 # ширина
@@ -66,6 +68,7 @@ class Player(pygame.sprite.Sprite):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
+        shoot_sound.play()
 
 
 class Mob(pygame.sprite.Sprite):
@@ -133,6 +136,13 @@ meteor_list = ['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_med1
                'meteorBrown_tiny1.png']
 for img in meteor_list:
     meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+# Загружаем все игровые звуки
+shoot_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav'))
+expl_sounds = []
+for snd in ['expl3.wav', 'expl6.wav']:
+    expl_sounds.append(pygame.mixer.Sound(path.join(snd_dir, snd)))
+pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.4)
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -144,6 +154,7 @@ for i in range(8):
     all_sprites.add(m)
     mobs.add(m)
 score = 0
+pygame.mixer.music.play(loops=-1)
 
 # Игровой цикл
 running = True
@@ -166,6 +177,7 @@ while running:
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     for hit in hits:
         score += 50 - hit.radius
+        random.choice(expl_sounds).play()
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
